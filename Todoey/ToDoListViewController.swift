e3658f0ec79faed81ee818a33a7913180e2ed7f7
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-09-27 19:25:30 +0300

    add UserDefaults

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    var list = ["FInd Mike", "Buy Eggs", "Kill sb"]
+    var itemArray = [String]()
+    let defaults = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.dataSource = self
         
+        //MARK: - code for nav bar color
         let appearance = UINavigationBarAppearance()
         appearance.configureWithOpaqueBackground()
         appearance.backgroundColor = .systemBlue
@@ -25,9 +26,42 @@ class ToDoListViewController: UITableViewController {
         navigationController?.navigationBar.tintColor = .white
         navigationController?.navigationBar.standardAppearance = appearance
         navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        tableView.dataSource = self
+        
+        guard let items = defaults.array(forKey: "ToDoList") as? [String] else {return}
+        itemArray = items
+        
+      
         
         
     }
+    
+    
+  
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell")
+        cell?.textLabel?.text = itemArray[indexPath.row]
+        return cell!
+        
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+       
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
     
     
     //MARK: - Add new items
@@ -41,10 +75,12 @@ class ToDoListViewController: UITableViewController {
         }
         
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
         alert.addAction(UIAlertAction(title: "Add", style: .default) { _ in
             guard !textField.text!.isEmpty else {return}
             let text = textField.text!
-            self.list.append(text)
+            self.itemArray.append(text)
+            self.defaults.set(self.itemArray, forKey: "ToDoList")
             self.tableView.reloadData()
           
             
@@ -53,29 +89,5 @@ class ToDoListViewController: UITableViewController {
         present(alert, animated: true)
             
 }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return list.count
-    }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell")
-        cell?.textLabel?.text = list[indexPath.row]
-        return cell!
-        
-    }
-
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(list[indexPath.row])
-       
-        
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
-
 }
 
